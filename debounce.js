0e3fea098edadd3b94f89b7204352fad66ec7185
mengfanfei <mengff0924@163.com>
2020-09-18 10:44:30 +0800

    整理

--- debounce.js
+++ debounce.js
@@ -1,9 +1,12 @@
 function debounce(func, wait, immediate) {
+    console.log(func,wait)
     let timeout, result;
     const debounced =  function() {
+        
         let context = this
         let args = arguments
         if(timeout) clearTimeout(timeout)
+        console.log(timeout)
         if(immediate) {
             let callNow = !timeout
             timeout = setTimeout(() => {
@@ -25,4 +28,35 @@ function debounce(func, wait, immediate) {
     }
     return debounced
 }
+// underscore官网
+// _.debounce = function(func, wait, immediate) {
+//     var timeout, args, context, timestamp, result;
 
+//     var later = function() {
+//       var last = _.now() - timestamp;
+
+//       if (last < wait && last > 0) {
+//         timeout = setTimeout(later, wait - last);
+//       } else {
+//         timeout = null;
+//         if (!immediate) {
+//           result = func.apply(context, args);
+//           if (!timeout) context = args = null;
+//         }
+//       }
+//     };
+
+//     return function() {
+//       context = this;
+//       args = arguments;
+//       timestamp = _.now();
+//       var callNow = immediate && !timeout;
+//       if (!timeout) timeout = setTimeout(later, wait);
+//       if (callNow) {
+//         result = func.apply(context, args);
+//         context = args = null;
+//       }
+
+//       return result;
+//     };
+//   };
